d2cd0ec4c4f2a4dfd40efb09ed227e7f0c9c8a2a
shahrambabaei <shahram.b6996@gmail.com>
2024-02-01 22:50:17 +0330

    fix:change

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,7 +1,6 @@
-import 'dart:developer';
+
 
 import 'package:camera/camera.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:test/showimagescreen.dart';
 
